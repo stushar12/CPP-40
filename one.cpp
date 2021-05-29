6f67611ac12125a3881f2d67f8488a45a16510a2
Tushar Saxena <stushar345@gmail.com>
2021-05-29 23:56:43 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -1,26 +1,42 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
- {
-    int p;
-    cin>>p;
-    while(p--)
- {
-     int n;
-     cin>>n;
-     int arr[n];
-     for(int i=0;i<n;i++)
-     {
-         cin>>arr[i];
-     }
-     int res=arr[0];
-     int maximum=arr[0];
-     for(int i=1;i<n;i++)
-     {
-         maximum=max(maximum+arr[i],arr[i]);
-         res=max(res,maximum);
-     }
-     
-     cout<<res<<endl;
- }
- }
+{
+    int arr[] = {5, 7, -3, 2, 9, 6, 16, 22, 21, 29, -14, 10, 12};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int res = arr[0];
+    int ans = arr[0];
+    int start = 0, end = 0, s = 0;
+    int finalstart = 0, finalend = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < res + arr[i])
+        {
+            res = res + arr[i];
+            start = s;
+            end = i;
+        }
+        else
+        {
+            res = arr[i];
+            start = i;
+            end = i;
+            s=i;
+        }
+        if (ans <= res)
+        {
+            ans = res;
+            finalstart = start;
+            finalend = end;
+        }
+        else
+        {
+            ans = ans;
+        }
+    }
+
+    cout <<"Longest contiguous subarray sum is: "<< ans << "\n";
+    cout << "Starting point is: "<<finalstart << " and  " <<"final point is: "<< finalend << endl;
+}
